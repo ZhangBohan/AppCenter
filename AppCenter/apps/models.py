1aae225b0994ddd3cc1d817518715ae1433db229
Bohan <bohan.zp@autonavi.com>
2017-08-25 20:12:04 +0800

    增加markdown支持

--- AppCenter/apps/models.py
+++ AppCenter/apps/models.py
@@ -1,3 +1,5 @@
+import markdown
+
 from django.db import models
 from model_utils.models import TimeStampedModel
 
@@ -6,7 +8,11 @@ class App(TimeStampedModel):
     name = models.CharField('名称', max_length=255)
     url_slug = models.CharField('URL', max_length=100, db_index=True)
     description = models.CharField('描述', max_length=1024, blank=True, null=True)
-    js_code = models.TextField('JS代码')
-    html_code = models.TextField('HTML代码')
-    css_code = models.TextField('CSS代码')
+    js_code = models.TextField('JS代码', blank=True, null=True)
+    html_code = models.TextField('HTML代码', blank=True, null=True)
+    css_code = models.TextField('CSS代码', blank=True, null=True)
+    readme_md = models.TextField('markdown 描述', blank=True, null=True)
     pv = models.IntegerField('访问量', default=0)
+
+    def markdown_to_html(self):
+        return markdown.markdown(self.readme_md)
